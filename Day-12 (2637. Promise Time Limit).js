e7a1cba83484d469666f348f22ab23ff2d3359fc
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-05-16 17:10:05 +0530

    Day-12 (2637. Promise Time Limit).js

---
+++ Day-12 (2637. Promise Time Limit).js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+  return async function(...args) {
+    // const promise = fn(...args);
+    // const timeout = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     reject("Time Limit Exceeded");
+    //   }, t);
+    // });
+    // return Promise.race([promise, timeout]);
+
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject("Time Limit Exceeded");
+        }, t);
+        fn(...args).then(resolve).catch(reject);
+    })
+  }
+};
+
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
